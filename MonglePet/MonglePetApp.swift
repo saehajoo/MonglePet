92cfc8c65013dd5c9a3eff837fdbcc95b5d3742f
saeha <saeha0109@naver.com>
2026-07-21 16:20:44 +0900

    Initial Commit

---
+++ MonglePet/MonglePetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonglePetApp.swift
+//  MonglePet
+//
+//  Created by netsprint on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MonglePetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
